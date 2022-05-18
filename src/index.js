b0aa4a063deb463f3ecb50cec4856d79c0873c70
Ross Keeley <ross.keeley@sky.uk>
2022-05-18 10:59:06 +0100

    added messages apis

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
-// importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-// defining the Express app
 const app = express();
 
-// defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'}
+const messages = [
+  {message: 'Hello hello hello'}
 ];
 
-// adding Helmet to enhance your API's security
 app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-
-// enabling CORS for all requests
 app.use(cors());
-
-// adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-// defining an endpoint to return all ads
-app.get('/', (req, res) => {
-  res.send(ads);
+// endpoint to return all messages
+app.get('/messages', (req, res) => {
+  res.send(messages);
 });
 
-// starting the server
+app.post('/messages', (req, res) => {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  res.send('message has been added');
+});
+
 app.listen(3001, () => {
   console.log('listening on port 3001');
 });
