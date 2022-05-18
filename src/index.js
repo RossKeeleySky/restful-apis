c091959a80fe32c220b980391177d35ea195f070
Ross Keeley <ross.keeley@sky.uk>
2022-05-18 12:41:41 +0100

    Created api routes for CRUD operations

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const { startDatabase } = require('./database/mongo');
+const { insertMessage, getMessages, deleteMessage, updateMessage, getMessageById } = require('./database/messages');
 
 const app = express();
-
-const messages = [
-  {message: 'Hello hello hello'}
-];
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -16,16 +14,35 @@ app.use(cors());
 app.use(morgan('combined'));
 
 // endpoint to return all messages
-app.get('/messages', (req, res) => {
-  res.send(messages);
+app.get('/messages', async (req, res) => {
+  res.send(await getMessages());
 });
 
-app.post('/messages', (req, res) => {
+app.get('/messages/:id', async (req, res) => {
+  res.send(await getMessageById(req.params.id));
+})
+
+app.post('/messages', async (req, res) => {
   const newMessage = req.body;
-  messages.push(newMessage);
-  res.send('message has been added');
+  await insertMessage(newMessage);
+  res.send('message inserted');
 });
 
-app.listen(3001, () => {
-  console.log('listening on port 3001');
+app.delete('/messages/:id', async (req, res) => {
+  await deleteMessage(req.params.id);
+  res.send('message deleted');
+});
+
+app.put('/messages/:id', async (req, res) => {
+  const updatedMessage = req.body;
+  await updateMessage(req.params.id, updatedMessage);
+  res.send('message updated');
+});
+
+startDatabase().then(async () => {
+  await insertMessage({title: 'Hello, now from the in-memory database!'});
+
+  app.listen(3001, async () => {
+    console.log('listening on port 3001');
+  });
 });
